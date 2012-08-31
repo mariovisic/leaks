dae42557c07417866541efbeda06fbee59e59f1b
Mario Visic <mario@mariovisic.com>
2012-08-31 23:32:44 +0800

    Spinners are neat.

--- lib/leaks/runner.rb
+++ lib/leaks/runner.rb
@@ -8,12 +8,19 @@ module Leaks
       @out       = verbose ? $stdout : nil
       @err       = $stderr
       @runs      = 0
+      @chars     = [ '|', '/', '-', '\\' ]
 
       ActiveRecord::Base.logger = nil
       RSpecRunner.run(@paths, @err, @out)
 
+      Thread.new do
+        loop do
+          sleep 0.1
+          print_status
+        end
+      end
+
       loop do
-        print_status
         RSpecRunner.run([], @err, @out)
 
         @runs +=1
@@ -21,12 +28,13 @@ module Leaks
     end
 
     def self.print_status
-      print 13.chr
+      print "#{13.chr}#{@chars[0]} "
+      @chars.push @chars.shift
 
       if @ram_usage.increased?
-        print "\033[31mRuns: #{@runs.to_s} #{@ram_usage}\033[0m"
+        print "\033[31m#{@ram_usage} Runs: #{@runs.to_s} \033[0m"
       else
-        print "\033[32mRuns: #{@runs.to_s} #{@ram_usage}\033[0m"
+        print "\033[32m#{@ram_usage} Runs: #{@runs.to_s} \033[0m"
       end
     end
   end
