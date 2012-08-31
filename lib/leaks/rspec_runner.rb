b36f097605f8e81e195400637034e15f63071f0e
Mario Visic <mario@mariovisic.com>
2012-08-31 23:23:58 +0800

    Do not instantiate a runner class.

--- lib/leaks/rspec_runner.rb
+++ lib/leaks/rspec_runner.rb
@@ -2,12 +2,14 @@ require 'rspec'
 
 module Leaks
   class RSpecRunner < RSpec::Core::Runner
-    def self.run(args, err=$stderr, out=nil)
+    def self.run(args, err, out)
       trap_interrupt
       options = RSpec::Core::ConfigurationOptions.new(args)
       options.parse_options
 
-      RSpec::Core::CommandLine.new(options).run(err, out)
+      unless RSpec::Core::CommandLine.new(options).run(err, out) == 0
+        raise RSpecError.new("A test has failed, quitting now. Run again in verbose mode to diagnose the problem")
+      end
     end
   end
 end
--- lib/leaks/runner.rb
+++ lib/leaks/runner.rb
@@ -1,25 +1,26 @@
 module Leaks
-  class Runner
-    def initialize(path = 'spec')
-      @path      = path
-      @ram_usage = RAMUsage.new
-      @runs      = 0
-    end
+  class RSpecError < StandardError; end
 
-    def run
+  class Runner
+    def self.run(path, verbose=false)
+      @ram_usage = RAMUsage.new
+      @paths     = Array(path || 'spec')
+      @out       = verbose ? $stdout : nil
+      @err       = $stderr
+      @runs      = 0
+
       ActiveRecord::Base.logger = nil
-      RSpecRunner.run([@path])
+      RSpecRunner.run(@paths, @err, @out)
 
       loop do
         print_status
-        RSpecRunner.run([])
+        RSpecRunner.run([], @err, @out)
+
         @runs +=1
       end
     end
 
-    private
-
-    def print_status
+    def self.print_status
       print 13.chr
 
       if @ram_usage.increased?
